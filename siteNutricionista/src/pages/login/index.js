ddf7901f82fba0f33218ee7d71aa8c2f02b75cbd
Luan Reinhold <anjobranco67@gmail.com>
2022-06-10 18:17:58 -0300

    add melhorias login

--- siteNutricionista/src/pages/login/index.js
+++ siteNutricionista/src/pages/login/index.js
@@ -1,25 +1,38 @@
 import './index.scss'
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+import LoadingBar from 'react-top-loading-bar'
+import { useNavigate } from 'react-router-dom'
+import { useState, useRef } from 'react'
+
 export default function Index() {
     
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
     const [erro, setErro] = useState('');
+    const [carregando, setCarregando] = useState('');
 
     const navigate = useNavigate();
-    
+    const ref = useRef();
+
     async function entrarClick() {
+        ref.current.continuousStart();
+        setCarregando(true);
         try {
             const r= await axios.post('http://localhost:5000/usuario/login', {
                 email: email, 
                 senha: senha
             })
+
+            setTimeout(() => {
                 navigate('/admin');
+            }, 3000);
+                
 
         } catch (err) {
+            ref.current.complete();
+            setCarregando(false);
             if (err.response.status === 401){
                 setErro(err.response.data.erro);
             }
@@ -28,6 +41,8 @@ export default function Index() {
 
     return (
         <main className="Page-Login">
+              <LoadingBar color='#2BC8A2' ref={ref} />
+
 
             <section class="faixa faixa1 fb-row">
             <img class="imagem-f1" src="/images/ffe3e452b6c0023c81707eba9375c69e.jpg" alt=""/>
@@ -35,16 +50,16 @@ export default function Index() {
             <h1 class="titulo">Acesse sua conta</h1>
             <div>
                 <h2 class="subtitulo"> E-mail</h2>
-                <input class="tag-input" type="text" name="email" id="" value={email} onChange={e => setEmail(e.target.value)} />
+                <input class="tag-input" type="text" name="email"  value={email} onChange={e => setEmail(e.target.value)} />
             </div>
             
             <div>
                 <h2 class="subtitulo">Senha</h2>
-                <input class="tag-input" type="password" name="senha" id="" value={senha} onChange={ e => setSenha(e.target.value)}/>
+                <input class="tag-input" type="password" name="senha"  value={senha} onChange={ e => setSenha(e.target.value)}/>
             </div>
 
 
-            <button className='botao'onClick={entrarClick}> Login </button>
+            <button className='botao'onClick={entrarClick} disabled={carregando}> Login </button>
             <div>
                 {erro}
             </div>
