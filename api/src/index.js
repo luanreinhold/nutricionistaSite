9ae940eca8c5fc6c733ffe68d48c4ae64cb03b98
Luan Reinhold <luanreinhold@gmail.com>
2022-06-15 11:46:30 -0300

    responsividade homepage

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,6 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
+import "dotenv/config"
+import express from "express"
+import cors from "cors"
 import admin from './controller/adminController.js'
 import paciente from './controller/pacienteController.js'
 
